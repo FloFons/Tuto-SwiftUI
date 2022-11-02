c43a3a02d2f4a9a58b5a0f9834a1edef5c180baa
Florent Fonsalas <florent.fonsalas@legrand.com>
2022-11-02 17:52:40 +0100

    Initial Commit

---
+++ Tuto SwiftUI/Tuto_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tuto_SwiftUIApp.swift
+//  Tuto SwiftUI
+//
+//  Created by Florent FONSALAS on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tuto_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
